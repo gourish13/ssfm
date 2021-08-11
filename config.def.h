e1ad2e4498a5df5e0d6e1625a80827ae19d13b4e
gourish13 <sadhu5gourish@outlook.com>
2021-08-12 02:34:18 +0530

    added config & selection of starting dir

---
+++ config.def.h
@@ -0,0 +1,9 @@
+/* Have a look at the LICENSE file for copyright and license details. */
+
+/*
+ * To make ssfm start always at a particular location when
+ * no location is passed as arguments set the value of 
+ * Start_Dir to the location as a string. For eg to make ssfm
+ * start alwas at home set Start_Dir = "/home/username";
+ */
+char *Start_Dir = NULL;
